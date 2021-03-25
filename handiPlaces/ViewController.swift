e6aa1de55e37c394a0af0d01e19ffdd14fe66d8b
tp <tp@tps-MacBook-Pro.local>
2021-03-25 18:09:34 +0100

    first commit

---
+++ handiPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handiPlaces
+//
+//  Created by tp on 25/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
