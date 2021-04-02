b6a9b67ca4f138ddee1e259ee4da0663c903390c
JeanPacomeDelmas <jeanpacome.delmas@hotmail.fr>
2021-04-02 04:52:42 -0700

    pb referencing

---
+++ handiPlaces/Model/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewCell.swift
+//  handiPlaces
+//
+//  Created by Jean-Pacôme Delmas on 02/04/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var handicapMentalCell: UIImageView!
+    @IBOutlet weak var handicapMoteurCell: UIImageView?
+    @IBOutlet weak var handicapVisuelCell: UIImageView?
+    @IBOutlet weak var handicapAuditifCell: UIImageView?
+    @IBOutlet weak var nameCell: UILabel!
+    @IBOutlet weak var adresseCell: UILabel!
+    @IBOutlet weak var villeEtDepartementCell: UILabel!
+    
+    @IBOutlet weak var test: UILabel!
+    
+}
